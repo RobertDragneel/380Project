044a0e08b9879c6690e5b76b0f0af53f3e9a0c91
RobertDragneel <robertstevenson007@gmail.com>
2023-09-21 15:47:23 -0700

    Started Customer class

--- Project.java
+++ Project.java
@@ -1,5 +1,14 @@
 public class Project {
     public static void main (String agrs[]) {
         System.out.println("Hello world!");
+
     }
+}
+
+class Customer
+{
+    String name;
+    String dateOfBirth;
+    
+
 }
