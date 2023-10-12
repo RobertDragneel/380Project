8d941f78338b16bbf565001ff3468a2d07b56032
jeh-loh <angelochristian.panopio.568@my.csun.edu>
2023-10-12 14:57:27 -0700

    GUI with drop down menu for flight search.

---
+++ airlinebooking/src/FlightFinderApp.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class FlightFinderApp {
+    private Frame frame;
+    private Choice originChoice;
+    private Choice destinationChoice;
+    private Button searchButton;
+
+    public FlightFinderApp() {
+        frame = new Frame("Flight Finder");
+        originChoice = new Choice();
+        destinationChoice = new Choice();
+        searchButton = new Button("Search");
+
+        // Adding destinations to the dropdowns
+        originChoice.add("Austin, Texas");
+        originChoice.add("Los Angeles, California");
+        originChoice.add("Portland, Oregon");
+        destinationChoice.add("New York City, New York");
+        destinationChoice.add("Chicago, Illinois");
+        destinationChoice.add("Miami, Florida");
+
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        frame.add(new Label("Select Origin:"), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        frame.add(originChoice, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        frame.add(new Label("Select Destination:"), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        frame.add(destinationChoice, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        frame.add(searchButton, gbc);
+
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String origin = originChoice.getSelectedItem();
+                String destination = destinationChoice.getSelectedItem();
+                System.out.println("Searching for flights from " + origin + " to " + destination);
+                // Perform flight search logic here
+            }
+        });
+
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new FlightFinderApp();
+    }
+}
