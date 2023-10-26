b923129fa32bf5e3fb78ed19c61875c3378b1429
RobertDragneel <robertstevenson007@gmail.com>
2023-10-26 15:44:05 -0700

    setting up Airplane class

--- airlinebooking/src/Airplane.java
+++ airlinebooking/src/Airplane.java
@@ -1,3 +1,16 @@
-public class Airplane {
-    
+public class Airplane 
+{
+    private int flightNumber;
+    private int departureTime;
+    private int arrivalTime;
+
+    public int getFlightNumber()
+    {
+        return flightNumber;
+    }
+
+    public void setFlightNumber()
+    {
+        
+    }
 }
