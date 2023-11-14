46021eb0d60e0002fe7a90cf939a46fb71850008
Alon Naiberg <alonn201@gmail.com>
2023-11-14 15:39:35 -0800

    changed flight data to be able to access arrays

--- airlinebooking/src/flightdata.java
+++ airlinebooking/src/flightdata.java
@@ -3,17 +3,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class flightdata {
-    public static String readExcelSheet() {
-        Workbook workbook = null;
+
+    public static List<String>[] readExcelData(String filePath) {
         try {
             // Load the Excel file
-            FileInputStream fis = new FileInputStream("C:\\Users\\pinoy\\Desktop\\CSUN\\COMP 380\\380Project\\flight_list.xlsx");
-            workbook = new XSSFWorkbook(fis);
+            FileInputStream fis = new FileInputStream(filePath);
+            Workbook workbook = new XSSFWorkbook(fis);
 
             // Get the first sheet in the workbook (you can specify a different sheet if needed)
             Sheet sheet = workbook.getSheetAt(0);
@@ -23,17 +23,13 @@ public class flightdata {
             int numCols = sheet.getRow(0).getPhysicalNumberOfCells();
 
             // Create an array list for each column
-            List<List<String>> columns = new ArrayList<>();
+            List<String>[] columns = new ArrayList[numCols];
             for (int i = 0; i < numCols; i++) {
-                columns.add(new ArrayList<>());
+                columns[i] = new ArrayList<>();
             }
-            
 
             // Create a date format
             SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
-
-            // StringBuilder used to concatenate the data
-            StringBuilder flightInfo = new StringBuilder();
 
             // Iterate through the rows and columns to populate the arrays
             for (int row = 1; row < numRows; row++) {
@@ -44,52 +40,36 @@ public class flightdata {
                         if (col == 5 || col == 7) {
                             // Format date and time values
                             Date dateValue = cell.getDateCellValue();
-                            //columns[col].add(dateFormat.format(dateValue));
-                            
-                            flightInfo.append(dateFormat.format(dateValue));
+                            columns[col].add(dateFormat.format(dateValue));
                         } else {
-                            //columns[col].add(cell.toString());
-
-                            flightInfo.append(cell.toString());
+                            columns[col].add(cell.toString());
                         }
                     } else {
-                        //columns[col].add(""); // Empty cell
-
-                        flightInfo.append(" ");
+                        columns[col].add(""); // Empty cell
                     }
-                    flightInfo.append(",");
                 }
-                flightInfo.append("\n");
             }
-
-            /* 
-            // You can now access the data in each column array
-            for (int col = 0; col < numCols; col++) {
-                System.out.println("Column " + (col + 1) + ": " + columns[col]);
-            }
-            */
-
 
             // Close the input stream
             fis.close();
-            
-            return flightInfo.toString();
+
+            return columns;
+
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (workbook != null) {
-                    workbook.close(); // Close the workbook in the finally block
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            return null;
+        }
+    }
+     
+    /*public static void main(String[] args) {
+        String filePath = "C:\\Users\\alonn\\OneDrive\\Desktop\\380\\380Project\\flight_list.xlsx";
+        List<String>[] dataColumns = readExcelData(filePath);
+
+        if (dataColumns != null) {
+            // You can now access the data in each column array
+            for (int col = 0; col < dataColumns.length; col++) {
+                System.out.println("Column " + (col + 1) + ": " + dataColumns[col]);
             }
         }
-
-        return null;
-    }
-    public static void main(String[] args) {
-        String excelData = readExcelSheet();
-        System.out.println(excelData);
-    }
+    }*/
 }
